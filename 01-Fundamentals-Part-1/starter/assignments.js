7535f831a6ba4e33ca6bc55346347173221dd7ed
skotancilar <60739606+skotancilar@users.noreply.github.com>
2020-11-03 22:05:59 +0300

    LECTURE 12:BASIC OPERATORS

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,6 @@
 //LECTURE 10: VALUES AND VARIABLES
-const country = 'Turkey', continent = 'Eurasia',
-    population = 80;
+country = 'Turkey', continent = 'Eurasia',
+    population = 85;
 
 console.log(country, continent, population);
 
@@ -16,3 +16,33 @@ console.log(typeof language);
 // LECTURE 12: LET,CONST,VAR
 language = 'Turkish';
 console.log(language);
+
+// BASIC OPERATORS
+//1
+function divideCountryPopulation(country, divideTo = 2) {
+    if (country === 'Turkey') {
+        console.log('Şehitler Ölmez, Vatan Bölünmez');
+    } else {
+        let newPopulation = population / divideTo;
+        console.log(newPopulation);
+    }
+};
+
+divideCountryPopulation('Turkey');
+divideCountryPopulation('Armenia', 323);
+//2
+population++;
+console.log(population);
+
+//3
+let populationFinland = 6;
+
+console.log(population > populationFinland);
+
+//4
+console.log(population > 33);
+
+//5
+
+let description = country + ' is in ' + continent + ' and it has ' + population + ' people speak ' + language;
+console.log(description);
