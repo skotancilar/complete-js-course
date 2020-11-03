483d41d7269efc88ff7a7aa6b7f1d7ad45957fe3
skotancilar <60739606+skotancilar@users.noreply.github.com>
2020-11-03 18:57:14 +0300

    LECTURE 11: DATA TYPES added and LEC 10 population corrected to number

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,14 @@
 //LECTURE 10: VALUES AND VARIABLES
 
-let country = 'Turkey', continent = 'Eurasia', population = '80 million';
+let country = 'Turkey', continent = 'Eurasia', population = 80;
 
 console.log(country, continent, population);
+
+// LECTURE 11: DATA TYPES
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
