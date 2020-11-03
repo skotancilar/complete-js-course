e49d2630537095e8c7d83ffda033bb58383e02a2
skotancilar <60739606+skotancilar@users.noreply.github.com>
2020-11-03 21:23:20 +0300

    LECTURE 12: LET,CONST,VAR

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,14 +1,18 @@
 //LECTURE 10: VALUES AND VARIABLES
-
-let country = 'Turkey', continent = 'Eurasia', population = 80;
+const country = 'Turkey', continent = 'Eurasia',
+    population = 80;
 
 console.log(country, continent, population);
 
 // LECTURE 11: DATA TYPES
-let isIsland = true;
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+// LECTURE 12: LET,CONST,VAR
+language = 'Turkish';
+console.log(language);
