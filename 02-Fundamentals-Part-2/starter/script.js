891a1ec3e99f3da0b51d712d3e3e87709708a42a
skotancilar <skotancilar@gmail.com>
2020-11-05 22:54:31 +0300

    CODING CHALLANGE 3 solution added

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -21,7 +21,7 @@ checkWinner();
 */
 
 /////////////// CODING CHALANGE 2////////////////
-let tip;
+/*let tip;
 const calcTip = (bill) => {
     let percentage;
     bill >= 50 && bill <= 300 ? percentage = .15 : percentage = .20
@@ -37,11 +37,58 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
-
+*/
+/*
 const Sefa = {
     firstName: 'Sefa',
     lastName: 'Kotancılar',
-    friends: ['Sena', 'Merve', 'Saleha']
+    friends: ['Sena', 'Merve', 'Saleha'],
+    birthYear: 1992,
+    hasDriversLicense: true,
+    job: 'pilot',
+    calcAge: function () {
+        this.age = 2020 - this.birthYear;
+        return this.age;
+    },
+    getsummary: function () {
+        return console.log(`${this.firstName} ${this.lastName} is a ${this.calcAge()} years old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : "no"} drivers licence.`);
+    }
 }
 
+Sefa.getsummary();
+
 console.log(`${Sefa.firstName} has ${Sefa.friends.length} friends, and his best friend is ${Sefa.friends[0]}`);
+*/
+
+/////////////// CODING CHALANGE 3 ////////////////
+
+const Mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = (this.mass / this.height ** 2).toFixed(2);
+        return this.bmi;
+    }
+}
+
+const John = {
+    fullName: 'John',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = (this.mass / this.height ** 2).toFixed(2);
+        return this.bmi;
+    }
+}
+Mark.calcBMI();
+John.calcBMI();
+console.log(Mark.bmi, John.bmi)
+
+if (Mark.bmi > John.bmi) {
+    console.log(`Mark's BMI ${Mark.bmi} is higher than John's BMI ${John.bmi}`);
+} else if (Mark.bmi < John.bmi) {
+    console.log(`John's ${John.bmi} is higher than Mark's BMI ${Mark.bmi}`);
+} else {
+    console.log('Both have same BMI');
+};
