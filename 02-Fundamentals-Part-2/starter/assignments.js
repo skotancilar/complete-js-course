736b07daf10025e454fb9e3eedce07ef62d2993a
skotancilar <skotancilar@gmail.com>
2020-11-05 15:12:46 +0300

    assignments.js LECTURE 34: FUNCTIONS DECLERATIONS VS. EXPANSIONS added

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,3 +1,5 @@
+//LECTURE 33: FUNCTIONS
+
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}
     `
@@ -9,3 +11,24 @@ const descUSA = describeCountry('USA', 300, 'Washington DC');
 
 console.log(descPakistan, descTurkey, descUSA);
 
+//LECTURE 34: FUNCTIONS DECLERATIONS VS. EXPANSIONS
+const worldPopulation = 7900;
+function percentageOfWorld1(population) {
+    return (population / worldPopulation) * 100;
+};
+
+let popTurkey = percentageOfWorld1(85);
+let popPakistan = percentageOfWorld1(200);
+let popUSA = percentageOfWorld1(300);
+
+console.log(popTurkey, popPakistan, popUSA);
+
+const percentageOfWorld2 = function (population) {
+    return Number(((population / worldPopulation) * 100).toFixed(2));
+};
+
+popTurkey = percentageOfWorld2(85);
+popPakistan = percentageOfWorld2(200);
+popUSA = percentageOfWorld2(300);
+
+console.log(popTurkey, popPakistan, popUSA);
