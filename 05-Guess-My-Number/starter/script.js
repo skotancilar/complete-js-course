3cc65ebac45bcb0d4d9c10d5ff5640462a7baaac
Sefa KOTANCILAR <skotancilar@gmail.com>
2020-11-08 15:29:34 +0300

    DOM elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+///DOMS
+const domBody = document.getElementsByTagName('body')
+const domAgain = document.querySelector('.again')
+const domNumber = document.querySelector('.number')
+const domGuess = document.querySelector('.quess');
+const domCheck = document.querySelector('.check');
+const domMessage = document.querySelector('.message');
+const domScore = document.querySelector('.score');
+const domHighScore = document.querySelector('.highscore')
+
