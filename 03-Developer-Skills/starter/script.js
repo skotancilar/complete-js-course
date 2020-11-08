09aa60aa411d637fb19bfa7937e24902c10c7e58
Sefa KOTANCILAR <skotancilar@gmail.com>
2020-11-08 11:17:07 +0300

    ... three dots is added string bug fixed

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -4,15 +4,16 @@
 const temps = [12, 5, -5, 0, 4]
 const temps2 = [17, 21, 23]
 const forecasts = []
+const dots = ['...']
 const printForecast = function (a, b) {
    const arr = a.concat(b)
    for (let i = 0; i < arr.length; i++) {
-      let el = `${arr[i]} ºC in ${i + 1} days...`;
+      let el = `${arr[i]} ºC in ${i + 1} days ...`;
       forecasts.push(el);
    }
-
+   let x = dots.concat(forecasts);
    for (let j = 0; j < forecasts.length; j++) {
-      console.log(` ${forecasts[j]}`);
+      console.log(...x);
    }
 }
 
