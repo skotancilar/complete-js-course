5aa9a068b7b0ae024fd2a991320187b120f891b2
Sefa KOTANCILAR <skotancilar@gmail.com>
2020-11-10 21:54:40 +0300

    +

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,34 @@
-'use strict';
+// 'use strict';
+// let age
+// function calcAge(birthYear) {
+//    age = 2020 - birthYear;
+//    return age;
+// }
+
+// calcAge(1992);
+// console.log(age);
+let x = 0;
+let y = 1;
+const sum = function () {
+   for (let i = 0; i < arguments.length; i++) {
+      x += arguments[i];
+   }
+   console.log(x);
+}
+
+const multiply = function () {
+   for (let i = 0; i < arguments.length; i++) {
+      y *= arguments[i];
+   }
+   console.log(y);
+}
+
+sum(20, 50, 40);
+multiply(2, 2);
+
+const age = 30;
+const oldAge = age;
+
+console.log(age);
+console.log(oldAge);
+
