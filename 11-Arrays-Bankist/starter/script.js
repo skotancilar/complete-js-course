d09c356d3e2fbca9a9d0fb714206732e023f5e26
Sefa KOTANCILAR <skotancilar@gmail.com>
2020-11-14 11:29:54 +0300

    Coding Challange #1 done

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
@@ -74,3 +74,55 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// for (const movement of movements) {
+for (const [i, mov] of movements.entries()) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+
+movements.forEach(foo);
+
+function foo(mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+}
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, xxx, map) {
+  console.log(`${map}: ${value}`);
+});
+
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+ */
+
+const juliasList = [9, 16, 6, 8, 3];
+const katesList = [10, 5, 6, 1, 4];
+
+var juliasCorectedList = juliasList.splice(1, 2);
+console.log(...juliasCorectedList);
+
+const fullList = juliasCorectedList.concat(katesList)
+console.log(...fullList);
+
+fullList.forEach(function (value, key) {
+  const str = `"Dog number ${key + 1} is ${value >= 3 ? `an adult, and is ${value} years old` : 'still puppy 🐶'}`
+  console.log(str);
+});
